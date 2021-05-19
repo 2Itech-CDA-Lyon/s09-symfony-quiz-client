01fd3c77e47fc83ef1ea99393a4569a693838db5
Romain Tello <romain.tello@gmail.com>
2021-05-19 16:33:43 +0200

    Add styling using React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Quiz } from './types/api';
 import useSWR from 'swr';
 import apiFetcher from './utils/api-fetcher';
+import { Button, Card, Container } from 'react-bootstrap';
 
 const App: FC = () => {
   const { data: quizzes, error } = useSWR<Quiz[], Error>(['GET', '/api/quiz'], apiFetcher);
@@ -11,16 +12,25 @@ const App: FC = () => {
   }
 
   return (
-    <>
-      <h2>Jouer</h2>
+    <Container>
+      <h1>Jouer</h1>
       <ul>
         {
           quizzes?.map(
-            quiz => <li key={quiz.id}>{quiz.title}</li>
+            quiz =>
+              <li key={quiz.id}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{quiz.title}</Card.Title>
+                    <Card.Text>{quiz.description}</Card.Text>
+                    <Button variant="primary">En voir plus</Button>
+                  </Card.Body>
+                </Card>
+              </li>
           )
         }
       </ul>
-    </>
+    </Container>
   );
 }
 
